972e0643bf95bc3f2c900230d642cf15b2f3ff61
__ibrahim__ <ibrahim.hindawi@robocomvr.com>
2021-05-05 15:22:47 +0400

    updated knim standalone + kincfile.js

--- kincfile.js
+++ kincfile.js
@@ -2,6 +2,11 @@ let project = new Project('Kinc');
 
 project.addFile('Sources/**');
 project.setDebugDir('Deployment');
-project.setDebugDir('DeploymentN');
+
+// PATH TO NIM LIBRARY
+project.addIncludeDir('C:/Users/Administrator/nim-1.4.2/lib');
+
+// PATH TO NIM CODEGEN
+project.addIncludeDir('./Knim-Standalone/cache');
 
 resolve(project);
