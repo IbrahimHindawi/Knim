49ba31fedfde0448324cd27be81c1dc0e16e53a2
ibra <ihindawi@gmail.com>
2021-05-14 19:39:12 +0400

    finished tutorial03 MVP

--- Sources/tutorial03/prog.nim
+++ Sources/tutorial03/prog.nim
@@ -14,11 +14,51 @@ import kinc/graphics4/indexbuffer
 import kinc/graphics4/shader
 import kinc/graphics4/constantlocation
 import kinc/math/matrix
-import kinc/math/vector
+#import kinc/math/vector
 import kinc/io/filereader
 import kinc/image
 import kinc/system
 import kinc/color
+
+import glm
+
+proc translate_matrix(kincMatrix: var kinc_matrix4x4_t, glmMatrix: Mat4x4): kinc_matrix4x4_t =
+  kincMatrix.m[0] = glmMatrix[0][0]
+  #echo kincMatrix.m[0]
+  kincMatrix.m[1] = glmMatrix[0][1]
+  #echo kincMatrix.m[1]
+  kincMatrix.m[2] = glmMatrix[0][2]
+  #echo kincMatrix.m[2]
+  kincMatrix.m[3] = glmMatrix[0][3]
+  #echo kincMatrix.m[3]
+  kincMatrix.m[4] = glmMatrix[1][0]
+  #echo kincMatrix.m[4]
+  kincMatrix.m[5] = glmMatrix[1][1]
+  #echo kincMatrix.m[5]
+  kincMatrix.m[6] = glmMatrix[1][2]
+  #echo kincMatrix.m[6]
+  kincMatrix.m[7] = glmMatrix[1][3]
+  #echo kincMatrix.m[7]
+  kincMatrix.m[8] = glmMatrix[2][0]
+  #echo kincMatrix.m[8]
+  kincMatrix.m[9] = glmMatrix[2][1]
+  #echo kincMatrix.m[9]
+  kincMatrix.m[10] = glmMatrix[2][2]
+  #echo kincMatrix.m[10]
+  kincMatrix.m[11] = glmMatrix[2][3]
+  #echo kincMatrix.m[11]
+  kincMatrix.m[12] = glmMatrix[3][0]
+  #echo kincMatrix.m[12]
+  kincMatrix.m[13] = glmMatrix[3][1]
+  #echo kincMatrix.m[13]
+  kincMatrix.m[14] = glmMatrix[3][2]
+  #echo kincMatrix.m[14]
+  kincMatrix.m[15] = glmMatrix[3][3]
+  #echo kincMatrix.m[15]
+  #echo "====="
+  result = kincMatrix
+
+
 
 var 
   vertices: seq[float32] = @[
@@ -38,7 +78,9 @@ var
   vertex_shader: kinc_g4_shader_t
   fragment_shader: kinc_g4_shader_t
 
-  mvp: kinc_matrix4x4_t
+  #mvp: kinc_matrix4x4_t = kinc_matrix4x4_identity()
+  mvp = mat4(1.0f)
+  kmvp: kinc_matrix4x4_t = kinc_matrix4x4_identity()
   mvpID: kinc_g4_constant_location_t
 
 proc load_shader(filename: cstring, shader: ptr kinc_g4_shader_t, shader_type: kinc_g4_shader_type_t) =
@@ -53,13 +95,14 @@ proc load_shader(filename: cstring, shader: ptr kinc_g4_shader_t, shader_type: k
 
 proc update {.cdecl.} =
   kinc_g4_begin(0)
-  kinc_g4_clear(kinc_g4_clear_color.cuint, KINC_COLOR_BLACK.cuint, 0.0f, 0)
+  kinc_g4_clear(kinc_g4_clear_color.cuint, KINC_COLOR_BLUE.cuint, 0.0f, 0)
 
   kinc_g4_set_vertex_buffer(vertexBuff.addr)
   kinc_g4_set_index_buffer(indexBuff.addr)
   kinc_g4_set_pipeline(pipe.addr)
 
-  kinc_g4_set_matrix4(mvpID, mvp.addr)
+  kmvp = translate_matrix(kmvp, mvp)
+  kinc_g4_set_matrix4(mvpID, kmvp.addr)
 
   kinc_g4_draw_indexed_vertices()
 
@@ -70,8 +113,8 @@ proc nim_start() {.exportc.} =
   discard kinc_init("Shader", 1024, 768, nil, nil)
   kinc_set_update_callback(update)
 
-  load_shader("shader.vert", vertex_shader.addr, KINC_G4_SHADER_TYPE_VERTEX)
-  load_shader("shader.frag", fragment_shader.addr, KINC_G4_SHADER_TYPE_FRAGMENT)
+  load_shader("mvp.vert", vertex_shader.addr, KINC_G4_SHADER_TYPE_VERTEX)
+  load_shader("mvp.frag", fragment_shader.addr, KINC_G4_SHADER_TYPE_FRAGMENT)
 
   var structure: kinc_g4_vertex_structure_t
   kinc_g4_vertex_structure_init(structure.addr)
@@ -90,16 +133,35 @@ proc nim_start() {.exportc.} =
 
   mvpID = kinc_g4_pipeline_get_constant_location(pipe.addr, "MVP")
 
-
   var
-    projection: kinc_matrix4x4_t = kinc_matrix4x4_perspective(45.0, 4.0/3.0, 0.1, 100.0)
-    view: kinc_matrix4x4_t = kinc_matrix4x4_look_at(kinc_vector3_t_new(4,3,3), kinc_vector3_t_new(0,0,0), kinc_vector3_t_new(0,1,0))
-    model: kinc_matrix4x4_t = kinc_matrix4x4_identity()
-    mvp: kinc_matrix4x4_t = kinc_matrix4x4_identity()
+    model = mat4(1.0f)
+    view = lookAt(vec3(4.0f, 4.0f, -3.0f), 
+                vec3(0.0f, 0.0f, 0.0f), 
+                vec3(0.0f, 1.0f, 0.0f))
+    projection = perspective(radians(45.0f), 4.0f/3.0f, 0.1f, 100.0f)
+
+  mvp = model * projection * view
+
   
-  mvp = kinc_matrix4x4_multiply(mvp.addr, projection.addr)
-  mvp = kinc_matrix4x4_multiply(mvp.addr, view.addr)
-  mvp = kinc_matrix4x4_multiply(mvp.addr, model.addr)
+  # var
+  #   ##mvp: kinc_matrix4x4_t = kinc_matrix4x4_identity() ## weirdness
+  #   projection: kinc_matrix4x4_t = kinc_matrix4x4_perspective(45.0, 4.0/3.0, 0.1, 100.0)
+  #   view: kinc_matrix4x4_t = kinc_matrix4x4_look_at(kinc_vector3_new(4,3,3), 
+  #                                                   kinc_vector3_new(0,0,0), 
+  #                                                   kinc_vector3_new(0,1,0))
+  #   model: kinc_matrix4x4_t = kinc_matrix4x4_identity()
+
+  
+  # mvp = kinc_matrix4x4_multiply(mvp.addr, projection.addr)
+  # ##mvp = kinc_matrix4x4_multiply(mvp.addr, view.addr)
+  # mvp = kinc_matrix4x4_multiply(mvp.addr, model.addr)
+  
+  
+  # for n in 0 ..< 4*4:
+  #   if n mod 4 == 0:
+  #     echo ""
+  #   stdout.write mvp[n], ", "
+  # echo ""
 
   kinc_g4_vertex_buffer_init(vertexBuff.addr, (vertices.len/3).cint, structure.addr, KINC_G4_USAGE_STATIC, 0)
   block:
